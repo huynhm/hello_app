5244f4cfbab943b2a3f7e4f60583c83b082de0a6
Michael Huynh <huynhm@umich.edu>
2017-03-26 22:59:37 -0400

    update models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+	belongs_to :user
+
 	validates :title, presence: true
 	validates :title, length: { minimum: 3, maximum: 15}
+	validates :user_id, presence: true
 end
